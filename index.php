03e071a5119973b87acf113622d5cf6691321a4e
Bouysse <florent.bouysse@oclock.school>
2024-01-06 12:05:35 +0100

    add begins project

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Florent Bouysse</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <nav>
+                <a class="logo" href="#">Logo</a>
+                <a href="#">Accueil</a>
+                <a href="#">A propos</a>
+                <a href="#">Mes projets</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header> 
+        <main>
+            <div class="container__main">
+                <div class="home main__size">
+                    <h1>Hey je suis Florent, développeur back-end Symfony</h1>
+                    <img class="profil__img" src="./assets/pictures/profil.jpg" alt="Florent Bouysse">
+                    <div class="container__home__img">
+                        <div class="div__img__home__1"></div>
+                        <div class="div__img__home__2"></div>
+                    </div>
+                </div>
+                <div class="about main__size">
+
+                </div>
+                <div class="project main__size">
+
+                </div>
+                <div class="contact main__size">
+
+                </div>
+            </div>
+            <div class="container__aside">
+                <div class="aside__home aside__size">
+                    <p>"Ceux qui réussissent trouve les moyens, quand les autres trouvent des excuses"</p>
+                    <p class="page__number">1-4</p>
+                </div>
+                <div class="aside__about aside__size">
+                    <p>"Ceux qui réussissent trouve les moyens, quand les autres trouvent des excuses"</p>
+                    <p class="page__number">2-4</p>
+                </div>
+                <div class="aside__project aside__size">
+                    <p>"Ceux qui réussissent trouve les moyens, quand les autres trouvent des excuses"</p>
+                    <p class="page__number">3-4</p>
+                </div>
+                <div class="aside__contact aside__size">
+                    <p>Parlez moi de vous et de votre projet</p>
+                    <p class="page__number">4-4</p>
+                </div>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+    </div>
+</body>
+</html>
